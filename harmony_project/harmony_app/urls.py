1ad8b4e5d877955f9ffed0cf83abc27cc1160446
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2023-06-04 12:40:22 -0500

    reiniciar

--- harmony_project/harmony_app/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.pantalla_login, name="login"),
-    path("pantalla_incial", views.pantalla_inicial, name="pantalla_inicial"),
-    path("pantalla_registro", views.pantalla_registro, name="pantalla_registro"),
-]
