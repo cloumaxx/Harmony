4c979f09cf4e76640ae74f270eea8031342ccde8
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2023-06-14 19:09:04 -0500

    comentarios listos

--- harmony/harmonyApp/urls.py
+++ harmony/harmonyApp/urls.py
@@ -12,5 +12,7 @@ urlpatterns = [
     path('pantalla_foro/<str:usuario_id>/', views.pantalla_foro, name='pantalla_foro'),
     path('pantalla_nuevo_comentario/<str:usuario_id>/', views.pantalla_nuevo_comentario, name='pantalla_nuevo_comentario'),
     path('pantalla_foro/<str:usuario_id>/comentario/<str:comentario_id>/incrementar_likes/', views.incrementar_likes, name='incrementar_likes'),
+    path('pantalla_foro/<str:usuario_id>/comentario/<str:comentario_id>/editar_comentario/', views.editar_comentario, name='editar_comentario'),
+    path('pantalla_foro/<str:usuario_id>/comentario/<str:comentario_id>/borrar_comentario/', views.borrar_comentario, name='borrar_comentario'),
 
 ]
