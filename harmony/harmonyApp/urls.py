aaa4614ecb900040bf0907619e8724efed197097
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2023-06-16 18:01:03 -0500

    but de url arreglado

--- harmony/harmonyApp/urls.py
+++ harmony/harmonyApp/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
     path("pantalla_incial/<str:usuario_id>/", views.pantalla_inicial, name="pantalla_inicial"),
     path("pantalla_registro", views.pantalla_registro, name="pantalla_registro"),
     path("pantalla_perfil_usuario/<str:usuario_id>/", views.pantalla_perfil_usuario, name="pantalla_perfil_usuario"),
-    path('actualizar_usuario/<str:usuario_id>/', views.actualizar_usuario, name='actualizar_usuario'),
+    path('actualizar_usuario/<str:usuario_obj>/', views.actualizar_usuario, name='actualizar_usuario'),
     
     path('pantalla_foro/<str:usuario_id>/', views.pantalla_foro, name='pantalla_foro'),
     path('pantalla_nuevo_comentario/<str:usuario_id>/', views.pantalla_nuevo_comentario, name='pantalla_nuevo_comentario'),
