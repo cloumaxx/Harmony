cee5d6ee6e1e47070d429af3e35c62efcccddb2b
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2023-07-10 13:45:52 -0500

    preparado para iniciar

--- harmony/harmonyApp/chatbot/modelo/modelo_bert.py
+++ harmony/harmonyApp/chatbot/modelo/modelo_bert.py
@@ -18,30 +18,25 @@ def definir_pregunta(pregunta):
 
     # Calcular la similitud de la pregunta con cada clave del JSON
     similarity_scores = {}
-    for key, value in json_data.items():
+    for entry in json_data["informacion"]:
         doc1 = nlpSpacy(pregunta.lower())
-        doc2 = nlpSpacy(' '.join(value).lower())
-        similarity_scores[key] = doc1.similarity(doc2)
+        doc2 = nlpSpacy(' '.join(entry["entradas"]).lower())
+        similarity_scores[entry["tag"]] = doc1.similarity(doc2)
 
     # Identificar la clave con la mayor similitud
     key_max_similarity = max(similarity_scores, key=similarity_scores.get)
 
     return key_max_similarity
-
 def respuesta_modelo_bert_contexto(pregunta):
     tipo_pregunta = definir_pregunta(pregunta)
-    print(">>>>>,tipo_pregunta",tipo_pregunta, type(tipo_pregunta))
-    print("pregunta:", pregunta, "clasificacion",definir_pregunta(pregunta))
+    #print("tipo_pregunta", tipo_pregunta, type(tipo_pregunta))
+    #print("pregunta:", pregunta, "clasificacion", definir_pregunta(pregunta))
     with open(ruta, 'r') as f:
         data = json.load(f)
-        contexto = data[tipo_pregunta]  # Extraer el campo 'titanic' de los datos JSON
-    if type(contexto) == list:
-        contexto = ", ".join(contexto)
-
-    print("contexto", contexto, type(contexto))
-
-    encode = tokenizer.encode_plus(contexto, return_tensors='pt')
-    input_ids = encode['input_ids'].tolist()
+        contexto = next(entry["respuestas"] for entry in data["informacion"] if entry["tag"] == tipo_pregunta)
+    #print("contexto:", contexto)
+    encode = tokenizer.encode_plus(pregunta, contexto, return_tensors='pt')
+    input_ids = encode['input_ids']
     tokens = tokenizer.convert_ids_to_tokens(input_ids[0])
        
     nlp = pipeline('question-answering', model=modelo, tokenizer=tokenizer)
