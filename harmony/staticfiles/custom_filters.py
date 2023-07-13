66440aba996df9533fa8feeb42e898c5f434551d
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2023-07-13 17:19:18 -0500

    a

--- harmony/staticfiles/custom_filters.py
+++
@@ -1,9 +0,0 @@
-from django import template
-
-register = template.Library()
-
-@register.filter
-def truncate_chars(value, length):
-    if len(value) <= length:
-        return value
-    return value[:length]
