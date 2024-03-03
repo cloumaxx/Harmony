45f41ee854d26a7bfceeb143dd2c8f0ea86b921e
Carlos_Eduardo_Alvear_Mutis_ <68306481+cloumaxx@users.noreply.github.com>
2024-03-03 17:57:15 -0500

    arreglado pruebas

--- harmony/harmonyApp/operations/utils.py
+++ harmony/harmonyApp/operations/utils.py
@@ -46,6 +46,7 @@ def get_comentariosVer(comentarios,db_connection):
         return comentarios_con_nombre_id
     except Exception as e:
         return None
+
 def get_inforeplicas(replica, db_connection):    
     try:
         usuario= db_connection.db.Usuario.find_one(
@@ -82,64 +83,29 @@ def enviar_correo_inicio_sesion(destinatario, nombre):
     # Crea el cuerpo del mensaje en formato HTML
     mensaje_html = f"""
     <html>
-
     <head>
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
-    <link href="https://getbootstrap.com/docs/5.3/assets/css/docs.css" rel="stylesheet">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
+        <title>Correo de Registro</title>
     </head>
-
     <body>
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4">
-                    <img src="cid:imagen">
-
-                </div>
-                <div class="col-md-8">
-                    <p>Hola, <b>{nombre}</b>.</p>
-                    <p>Has iniciado sesión exitosamente en HarmonyApp.</p>
-                </div>
-            </div>
-
-        </div>
-
+        <p>Hola, <b>{nombre}</b>.</p>
+        <p>Has iniciado sesión exitosamente en HarmonyApp.</p>
     </body>
-
     </html>
     """
     
     # Adjunta el mensaje HTML al correo
     msg.attach(MIMEText(mensaje_html, 'html'))
 
-    # Descarga la imagen desde la URL
-    imagen_url = 'https://i.imgur.com/x3plOXF.png'
-    imagen_respuesta = requests.get(imagen_url)
-    imagen_data = imagen_respuesta.content
-
-    # Adjunta la imagen al correo y asigna un CID para referenciarla en el HTML
-    imagen_adjunta = MIMEImage(imagen_data)
-    imagen_adjunta.add_header('Content-Disposition', 'attachment; filename="imagen.png"')
-    imagen_adjunta.add_header('Content-ID', '<imagen>')
-    msg.attach(imagen_adjunta)
-
     # Inicia una conexión SMTP segura y envía el correo
     try:
-        server = smtplib.SMTP(smtp_server, smtp_port)
-        server.starttls()
-        server.login(remitente, contraseña)
-        server.sendmail(remitente, destinatario, msg.as_string())
-        server.quit()
+        with smtplib.SMTP(smtp_server, smtp_port) as server:
+            server.starttls()
+            server.login(remitente, contraseña)
+            server.send_message(msg)
         return "Correo electrónico enviado con éxito."
     except Exception as e:
         return "Error al enviar el correo electrónico: " + str(e)
-
-
+  
 def detectarStopWords(texto_a_analizar):
     # Carga el modelo de spaCy para español
     nlp = spacy.load('es_core_news_sm')
--- harmony/harmonyApp/tests.py
+++ harmony/harmonyApp/tests.py
@@ -1,52 +1,65 @@
+import random
 from django.test import TestCase, RequestFactory
 from django.contrib.auth.models import User
 from django.urls import reverse
 from unittest.mock import patch
 from harmonyProject.database import MongoDBConnection
-from harmonyApp.models import Credenciales, Usuario
+from harmonyApp.models import Credenciales
 from harmonyApp.views import pantalla_registro
-from harmonyApp.operations.utils import comunicacionMillyApi
+from harmonyApp.operations.utils import cifrarClaves, comunicacionMillyApi, decifrarClaves
 
 class PantallaRegistroTestCase(TestCase):
     db_connection = MongoDBConnection()
-
-    def setUp(self):
-        # Configuración inicial para las pruebas
-        self.factory = RequestFactory()
-        #self.user = User.objects.create_user(username='testuser', email='samymartsa@yahoo.com', password='aaaa')
 
     def test_pantalla_registro(self):
         # Crear una solicitud POST para la vista con datos de formulario simulados
         data = {
             'nombre': 'John',
             'apellido': 'Doe',
-            'correo': 'samymartsa@yahoo.com',
+            'correo': 'prueba'+str(random.randint(0, 150))+'@yahoo.com',
             'clave': 'testpassword',
             'genero': 'M',
-            'fecha_nacimiento': '1990-01-01',  # Formato: Año-Mes-Día
+            'fecha_nacimiento': '1990-01-01',  
+            "url_imagen_perfil": "https://i.imgur.com/0RW7b5J.jpg",
+            "code_delete_img": "",
+            "conversaciones": []
         }
+        
+    
+        # Simular la existencia del correo en la base de datos antes de realizar la solicitud
         existeCorreo = self.db_connection.db.Credenciales.find_one({'correo': data['correo']})
-        self.assertTrue(existeCorreo)
+        self.assertFalse(existeCorreo)  # El correo no debe existir antes de la solicitud
+        
+        # Realizar la solicitud a la vista de registro
+        response = self.client.post(reverse('pantalla_registro'), data)
+        
+        # Verificar que la solicitud fue exitosa (código de estado 200)
+        self.assertEqual(response.status_code, 302)
+        
+        # Verificar que se haya creado una credencial para el usuario
+        existeCorreoDespues = self.db_connection.db.Credenciales.find_one({'correo': data['correo']})
+        self.assertTrue(existeCorreoDespues)  # El correo debe existir después de la solicitud
 
-        # Verificar la respuesta y el comportamiento esperado
-        #self.assertEqual(response.status_code, 302)  # Comprobar redireccionamiento
+    def test_validar_cifrado(self):
+        """
+        Verifica que la contraseña sea cifrada correctamente
+        """
+        # Configuración inicial para las pruebas
+        clave = 'testpassword'
 
+        clave_cifrada = cifrarClaves(clave)
+        clave_descifrada = decifrarClaves(clave_cifrada)
 
-        # Verificar que se envió el correo de confirmación (puedes simular esto utilizando mocks)
-
+        self.assertEquals(clave, clave_descifrada)
 
 class TestComunicacionMillyApi(TestCase):
     def test_comunicacion_milly_api_success(self):
-        
+        # Probar comunicación exitosa con la API
         respuesta = comunicacionMillyApi('cual es tu nombre')
-        self.assertNotEqual(respuesta, 'No entendi tu pregunta')
+        self.assertNotEqual(respuesta, 'No entendí tu pregunta')
 
-"""
-    def test_comunicacion_milly_api_failure(self):
-        # Configurar el mock para simular una respuesta fallida de la API
-        
-        # Llamar a la función con una pregunta
-        respuesta = comunicacionMillyApi('Hola, ¿cómo estás?')
-
-        # Verificar que la función devuelva el mensaje de error
-        self.assertEqual(respuesta, 'No entendí tu pregunta')"""
+    """def test_comunicacion_milly_api_failure(self):
+        # Probar comunicación fallida con la API
+        respuesta = comunicacionMillyApi('')
+        self.assertEqual(respuesta, 'No entendí tu pregunta')
+    """
